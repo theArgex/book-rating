0cfbff6fe637881549affaf38125a7d817920060
alexander grädel <alexander.graedel@swisscom.com>
2021-05-05 14:40:50 +0200

    add documentation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { Book } from './../shared/book';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  books = ['Angular', 'React', 'Vue', 'The Hitchhikers Guide to the Galaxy'];
+  books: Book[] = [
+  {
+    isbn: '000',
+    title: 'Angular',
+    description: 'Buch über Angular',
+    rating: 5
+  },
+  {
+    isbn: '001',
+    title: 'React',
+    description: 'Buch über React',
+    rating: 3
+  },
+  {
+    isbn: '002',
+    title: 'Vue',
+    description: 'Buch über Vue',
+    rating: 4
+  },
+  {
+    isbn: '003',
+    title: 'The Hitchhikers Guide to the Galaxy',
+    description: '42',
+    rating: 5
+  }
+  ];
 
   constructor() { }
 
