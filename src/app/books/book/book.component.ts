a5d116b33ca84c3de5b67ca3277417040f4d7a1b
alexander grädel <alexander.graedel@swisscom.com>
2021-05-06 11:47:07 +0200

    open books api

--- src/app/books/book/book.component.ts
+++ src/app/books/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
@@ -8,6 +8,25 @@ import { Book } from '../shared/book';
 })
 export class BookComponent {
 
+  @Output()
+  rateUp = new EventEmitter<Book>();
+
+  @Output()
+  rateDown = new EventEmitter<Book>();
+
   @Input() book: Book;
 
+  emptyStars: number;
+
+  getEmptyStars(booksRating: number): number {
+    return (this.emptyStars = 5 - booksRating);
+  }
+
+  doRateUp(): void {
+    this.rateUp.emit(this.book);
+  }
+
+  doRateDown(): void {
+    this.rateDown.emit(this.book);
+  }
 }
--- src/app/books/dashboard/dashboard.component.ts
+++ src/app/books/dashboard/dashboard.component.ts
@@ -10,25 +10,25 @@ export class DashboardComponent implements OnInit {
 
   books: Book[] = [
   {
-    isbn: '000',
+    isbn: '3864906466',
     title: 'Angular',
     description: 'Buch über Angular',
     rating: 5
   },
   {
-    isbn: '001',
+    isbn: '0991344626',
     title: 'React',
     description: 'Buch über React',
     rating: 3
   },
   {
-    isbn: '002',
+    isbn: '1925836096',
     title: 'Vue',
     description: 'Buch über Vue',
     rating: 4
   },
   {
-    isbn: '003',
+    isbn: '1491933178',
     title: 'The Hitchhikers Guide to the Galaxy',
     description: '42',
     rating: 5
@@ -38,6 +38,14 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  doRateUp(): void {
+    debugger
+  }
+
+  doRateDown(): void {
+    debugger
   }
 
 }
