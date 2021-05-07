eb65a071571e022852a4455c3730daa461092db2
alexander grädel <alexander.graedel@swisscom.com>
2021-05-07 13:11:00 +0200

    fix unit test

--- src/app/books/dashboard/dashboard.component.spec.ts
+++ src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,17 +1,32 @@
+import { BookRatingService } from './../shared/book-rating.service';
 import { BookComponent } from './../book/book.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { Book } from '../shared/book';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    const bookRatingMock = {
+      rateUp: book => book,
+      checkDisableRateUp: () => true,
+      checkDisableRateDown: () => true
+    };
+
     await TestBed.configureTestingModule({
       declarations: [
         DashboardComponent,
         BookComponent
+      ],
+      providers: [
+        {
+          provide: BookRatingService,
+          useValue: bookRatingMock
+        }
       ]
     })
     .compileComponents();
@@ -25,5 +40,17 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('doRateUp() should forward all calls to BookRatingService', () => {
+
+    const rs = TestBed.inject(BookRatingService);
+
+    spyOn(rs, 'rateUp').and.callThrough();
+
+    const testBook = { isbn: '000' } as Book;
+    component.doRateUp(testBook);
+
+    expect(rs.rateUp).toHaveBeenCalledOnceWith(testBook);
   });
 });
