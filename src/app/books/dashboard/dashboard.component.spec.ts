97adb4cbf1ed4a5165428643d036c123b10f6d38
alexander grädel <alexander.graedel@swisscom.com>
2021-05-07 11:29:22 +0200

    add bookcomponent

--- src/app/books/dashboard/dashboard.component.spec.ts
+++ src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,3 +1,4 @@
+import { BookComponent } from './../book/book.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
@@ -8,7 +9,10 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [
+        DashboardComponent,
+        BookComponent
+      ]
     })
     .compileComponents();
   });
